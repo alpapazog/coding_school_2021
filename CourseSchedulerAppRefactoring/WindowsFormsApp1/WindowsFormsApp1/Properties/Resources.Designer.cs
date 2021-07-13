48c3ce43ab4d9330e7e33d8ce349ec515351fa21
Alexis Papazoglou <alpapazog@gmail.com>
2021-07-13 16:11:23 +0300

    removed useless resource

--- CourseSchedulerAppRefactoring/WindowsFormsApp1/WindowsFormsApp1/Properties/Resources.Designer.cs
+++ CourseSchedulerAppRefactoring/WindowsFormsApp1/WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace UniversityApp.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap token_1 {
-            get {
-                object obj = ResourceManager.GetObject("token_1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
